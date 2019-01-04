13126cc25cfc170689285dd181ce6bffe717e22e
mzainiulilabshor <45529539+mzainiulilabshor@users.noreply.github.com>
2019-01-05 01:57:55 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main () {
+    int i,n;
+
+cout << " Menampilkan Kata Algoritma Sebanyak N Kali "<< endl;
+cout << " Masukan Jumlah Kata Yang ingin di tampilkan : ";cin>>n;
+
+
+
+for ( i=1;i<=n;i++)
+{
+    cout << " Algoritma " <<endl;
+}
+getch();
+return 0;
+}
